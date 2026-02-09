69614eda3589a5ae593a588029b2791a17e40399
Simon Marin <simon.marin.m@hotmail.com>
2026-02-09 16:06:52 -0500

    dev: removing worker service

--- src/modules/work/work.service.js
+++ src/modules/work/work.service.js
@@ -1,19 +1,15 @@
 
-import { listDueCallCases, scheduleDueCallCases } from '../../jobs/schedule-due-cases.js';
-
 export const workService = {
     runWorker: async (tenantId, options = {}) => {
         const { limit = 50, perTenantLimit, dryRun = false } = options;
 
-        if (dryRun) {
-            const cases = await listDueCallCases({ tenantId, limit, perTenantLimit });
-            return {
-                dryRun: true,
-                found: cases.length,
-                cases,
-            };
-        }
-
-        return await scheduleDueCallCases({ tenantId, limit, perTenantLimit });
+        return {
+            disabled: true,
+            tenantId,
+            limit,
+            perTenantLimit,
+            dryRun,
+            message: 'Worker scheduling was moved to the mordecai-workers service.',
+        };
     }
 };
